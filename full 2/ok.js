eee8f8948339dda22f28bd4d4ab875f1831f7c78
RohanPayamalle-01 <143057650+RohanPayamalle-01@users.noreply.github.com>
2023-08-25 09:09:57 +0530

    desktop

---
+++ full 2/ok.js
@@ -0,0 +1,17 @@
+let ok = [12,21]
+console.log(ok)
+console.log(ok[0])
+console.log(ok[1])
+ok.push (6)
+console.log(ok)
+console.pop() 
+console.log(ok)
+console.log(ok[1]+ok[0])
+for(let i=0;i<500;i++)
+{
+console.log(i){
+    console.log("chosen 50")
+    continue
+
+}
+}
